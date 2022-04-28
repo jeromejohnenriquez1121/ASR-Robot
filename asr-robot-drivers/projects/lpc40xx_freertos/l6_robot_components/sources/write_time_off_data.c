5e3111052968afe4530ab000b1a5a39999278010
Jerome Enriquez <jeromej.enriquez@gmail.com>
2022-04-28 02:53:30 -0700

    Add debug printf statements to write_time_off/on.c

--- asr-robot-drivers/projects/lpc40xx_freertos/l6_robot_components/sources/write_time_off_data.c
+++ asr-robot-drivers/projects/lpc40xx_freertos/l6_robot_components/sources/write_time_off_data.c
@@ -1,29 +1,34 @@
 #include "write_time_off_data.h"
 
 #include "FreeRTOS.h"
+#include "delay.h"
 #include "queue.h"
 #include "string.h"
+#include <stdio.h>
 
 #include "ff.h"
 
 QueueHandle_t time_off_data_queue;
 
 void write_time_off_data__freertos_task(void *parameter) {
-  char line[50] = "";
+  char line[12] = "";
   FIL file;
 
   while (1) {
     xQueueReceive(time_off_data_queue, &line[0], portMAX_DELAY);
-    strcat(&line[0], "ON\n");
+    vTaskDelay(500);
+
+    printf("Received %s\n", &line[0]);
+    strcat(&line[0], "OFF\n");
 
     UINT bytes_written = 0;
     FRESULT f_result = f_open(&file, "asr.txt", (FA_WRITE | FA_OPEN_APPEND));
 
     if (f_result == FR_OK) {
-      f_write(&file, &line[0], strlen(line), &bytes_written);
+      f_write(&file, &line[0], strlen(&line[0]), &bytes_written);
     }
 
-    memset(&line[0], 0, strlen(line));
+    memset(&line[0], 0, strlen(&line[0]));
 
     f_close(&file);
   }
--- asr-robot-drivers/projects/lpc40xx_freertos/l6_robot_components/sources/write_time_on_data.c
+++ asr-robot-drivers/projects/lpc40xx_freertos/l6_robot_components/sources/write_time_on_data.c
@@ -2,6 +2,7 @@
 #include "ff.h"
 
 #include "FreeRTOS.h"
+#include "delay.h"
 #include "queue.h"
 
 #include <stdint.h>
@@ -11,21 +12,24 @@
 QueueHandle_t time_on_data_queue;
 
 void write_time_on_data__freertos_task(void *parameter) {
-  char line[10] = "";
+  char line[12] = "";
   FIL file;
 
   while (1) {
     xQueueReceive(time_on_data_queue, &line[0], portMAX_DELAY);
+    vTaskDelay(500);
+
+    printf("Received %s\n", &line[0]);
     strcat(&line[0], "ON\n");
 
     UINT bytes_written = 0;
     FRESULT f_result = f_open(&file, "asr.txt", (FA_WRITE | FA_OPEN_APPEND));
 
     if (f_result == FR_OK) {
-      f_write(&file, &line[0], strlen(line), &bytes_written);
+      f_write(&file, &line[0], strlen(&line[0]), &bytes_written);
     }
 
-    memset(&line[0], 0, strlen(line));
+    memset(&line[0], 0, strlen(&line[0]));
 
     f_close(&file);
   }
